1c4f0f6854137edf3b810c8ef69a209a1e886e5b
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2012-03-22 16:24:29 +0100

    bring your own magento

--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -15,25 +15,6 @@ class { "mysql":
 }
 
 /**
- * Magento config
- */
-class { "magento":
-    /* magento version */
-    version        => "1.6.1.0",
-
-    /* magento database settings */
-    db_username    => "magento",
-    db_password    => "magento",
-
-    /* magento admin user */
-    admin_username => "admin",
-    admin_password => "123123abc",
-
-    /* "yes|no */
-    use_rewrites   => "no",
-}
-
-/**
  * Import modules
  */
 include apt
