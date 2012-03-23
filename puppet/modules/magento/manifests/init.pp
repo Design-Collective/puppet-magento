e16810c759462e5c02b726327587f07d8546830a
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2012-03-23 12:06:04 +0100

    fix vhost, add rewrite module since magento needs it

--- puppet/modules/magento/manifests/init.pp
+++ puppet/modules/magento/manifests/init.pp
@@ -1,10 +1,17 @@
 class magento {
 
-   file { "/etc/apache2/sites-available/magento":
+   file { "/etc/apache2/sites-available/vagrant-magento":
      source => 'puppet:///modules/magento/vhost_magento'
+   }
+
+   file { "/etc/apache2/sites-enabled/vagrant-magento":
+     ensure => 'link',
+     target => '/etc/apache2/sites-available/vagrant-magento'
    }
 
    file { "/var/log/apache2/magento":
      ensure => 'directory'
    }
+
+   exec { "/usr/sbin/a2enmod rewrite": }
 }
